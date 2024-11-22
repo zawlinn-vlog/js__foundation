723a17bc99363b3d72afb39d9cbf6f02fd468cb3
zawlinn-profile <zawlinn.profile@gmail.com>
2024-11-22 14:49:28 +0700

    JavaScript: JS Foundation Update

--- 06 Array Methods/02 Project/assets/js/app.js
+++ 06 Array Methods/02 Project/assets/js/app.js
@@ -1,18 +1,18 @@
-"use strict";
+'use strict';
 
 // GET DOM ELEMENTs
 
-const loginUsername = document.querySelector("#loginUserName");
-const loginUserLabel = document.querySelector("#loginUserLabel");
-const loginPIN = document.querySelector("#loginPin");
-const loginPinLabel = document.querySelector("#loginPinLabel");
-const submit__l = document.querySelector("#login__submit");
-const userDataContainer = document.querySelector(".user__data");
+const loginUsername = document.querySelector('#loginUserName');
+const loginUserLabel = document.querySelector('#loginUserLabel');
+const loginPIN = document.querySelector('#loginPin');
+const loginPinLabel = document.querySelector('#loginPinLabel');
+const submit__l = document.querySelector('#login__submit');
+const userDataContainer = document.querySelector('.user__data');
 
 // DATA
 
 const account1 = {
-  owner: "Jonas Schmedtmann",
+  owner: 'Jonas Schmedtmann',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2,
   pin: 1111,
@@ -21,52 +21,52 @@ const account1 = {
 // createEl(account1.movements);
 
 const account2 = {
-  owner: "Jessica Davis",
+  owner: 'Jessica Davis',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
   pin: 2222,
 };
 
 const account3 = {
-  owner: "Steven Thomas Williams",
+  owner: 'Steven Thomas Williams',
   movements: [200, -100, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
-  owner: "Smith William",
+  owner: 'Smith William',
   movements: [100, -50, 430, -320, -25, 500, 4000, -450],
   interestRate: 0.7,
   pin: 4444,
 };
 
 const curriencies = new Map([
-  ["USD", "United States Dollar"],
-  ["EUR", "Euro"],
-  ["GBP", "Pound Sterling"],
+  ['USD', 'United States Dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound Sterling'],
 ]);
 
 const accounts = [account1, account2, account3, account4];
 
-const createUserDataLists = (acc) => {
+const createUserDataLists = acc => {
   // console.log(acc);
-  userDataContainer.innerHTML = "";
+  userDataContainer.innerHTML = '';
   acc.movements.forEach((mov, i) => {
     let html = `
 
     <div class="user__items d__flex">
       <div class="user__action">
-        <span class="user__badge user__${mov > 0 ? "deposite" : "withdraw"}"> ${
+        <span class="user__badge user__${mov > 0 ? 'deposite' : 'withdraw'}"> ${
       i + 1
-    } ${mov > 0 ? "deposite" : "withdraw"}</span>
+    } ${mov > 0 ? 'deposite' : 'withdraw'}</span>
         <span class="user__action-date">31/07/2024</span>
       </div>
       <p class="user__amounts">${mov}€</p>
     </div>
 `;
 
-    userDataContainer.insertAdjacentHTML("afterbegin", html);
+    userDataContainer.insertAdjacentHTML('afterbegin', html);
   });
 };
 
@@ -75,12 +75,12 @@ createUserDataLists(account1);
 /*  CREATE USER NAME FOR ALL ACCOUNTs */
 
 (() => {
-  accounts.map((acc) => {
+  accounts.map(acc => {
     let userName = acc.owner
       .toLowerCase()
-      .split(" ")
-      .map((n) => n[0])
-      .join("");
+      .split(' ')
+      .map(n => n[0])
+      .join('');
 
     acc.userName = userName;
 
@@ -89,66 +89,3 @@ createUserDataLists(account1);
 })();
 
 // console.log(accounts);
-
-/*   CHECK LOGIN   */
-
-let currentAccount,
-  loginUser = {};
-
-/*   create custom css */
-
-const cusCss = (el1, el2, cur) => {
-  el1.textContent = `PIN`;
-  el1.style.color = "#39b385";
-  el2?.removeAttribute("disabled");
-  el2.style.cursor = cur || "not-allow";
-
-  console.log(cur);
-};
-
-const checkUser = () => {
-  // e.preventDefault();
-
-  const inputUsername = loginUsername.value.trim();
-
-  currentAccount = accounts.find((acc) => acc.userName === inputUsername);
-
-  if (!currentAccount) {
-    loginUserLabel.textContent = `wrong user`;
-    loginUserLabel.style.color = "#e52a5a";
-
-    return false;
-  }
-
-  loginUser.userName = inputUsername;
-
-  cusCss(loginUserLabel, loginPIN);
-
-  // loginUserLabel.textContent = `Username`;
-  // loginUserLabel.style.color = "#39b385";
-
-  // loginPIN.removeAttribute("disabled");
-
-  console.log(currentAccount);
-};
-
-loginPIN.addEventListener("keyup", () => {
-  const inputPIN = Number(loginPIN.value);
-
-  console.log(currentAccount.pin === inputPIN);
-
-  if (currentAccount?.pin !== inputPIN) {
-    loginPinLabel.textContent = `wrong Pin`;
-    loginPinLabel.style.color = "#e52a5a";
-    // console.log("wrong pin");
-    return false;
-  }
-
-  loginUser.pin = inputPIN;
-
-  cusCss(loginPinLabel, submit__l, "pointer");
-
-  // submit__l.style.cursor = "pointer";
-});
-
-loginUsername.addEventListener("keyup", checkUser);

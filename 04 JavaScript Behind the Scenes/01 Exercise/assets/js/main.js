12be1c316db42ac37d4721eba0a81190cf9f3639
zawlinn-vlog <zawlinn.profile@outlook.com>
2025-05-10 13:18:00 +0700

    JavaScript: Basic to Adv

--- 04 JavaScript Behind the Scenes/01 Exercise/assets/js/main.js
+++ 04 JavaScript Behind the Scenes/01 Exercise/assets/js/main.js
@@ -122,3 +122,126 @@ console.log(fname, currentAge);
 
 // console.log(`Value of x: ${x}`);
 // console.log(`Value of y: ${y}`);
+
+let num1 = 100;
+let num2 = num1;
+
+console.log(num1);
+console.log(num2);
+
+num1 = 101;
+
+console.log(num1);
+console.log(num2);
+
+let addressSmith = {
+  address: "No.482, Oak Pho Dan Street, Auk Kyin Qr, Thaton",
+  phone: "057-400256",
+};
+
+let addressWilliam = addressSmith;
+
+console.log(addressSmith, addressWilliam);
+
+addressSmith.phone = "057-400405";
+
+console.log(addressSmith, addressWilliam);
+
+// Destructor Array and Object
+
+// Array
+
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic", "Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+  openingHours: {
+    thu: {
+      open: 12,
+      closed: 22,
+    },
+    fri: {
+      open: 11,
+      closed: 23,
+    },
+    sat: {
+      open: 0, // 24 hours
+      closed: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = "20:00",
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}.`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your declicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
+  },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient, otherIngredients);
+  },
+};
+
+// const [mainMenu, subMenu] = restaurant.mainMenu;
+// const [mainMenu, , subMenu] = restaurant.mainMenu;
+
+// console.log(mainMenu, subMenu);
+
+// const { starterMenu, mainMenu } = restaurant;
+const { starterMenu: sMenu, mainMenu: mMenu } = restaurant;
+
+// console.log(sMenu, mMenu);
+
+// Rest Operator Array
+
+const [first, last, ...other] = restaurant.starterMenu;
+
+console.log(first, last, other);
+
+// Rest Operator Object ...
+
+const { starterMenu, name, openingHours, ...rest } = restaurant;
+
+console.log(starterMenu, name, openingHours, rest);
+
+// Spreat Operator ...
+
+const newMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(newMenu);
+
+const newRestaurant = { founder: "Saram Khan", ...restaurant };
+console.log(restaurant, newRestaurant);
+
+console.log(...restaurant.starterMenu);
+
+// SHADOW COPY
+
+// const newarr = newMenu;
+
+// newMenu.push("Fry Noodle");
+
+// console.log(newMenu);
+// console.log(newarr);
+
+const newarr = [...newMenu];
+
+newarr.push("Fry Noodle");
+
+console.log(newMenu);
+console.log(newarr);
